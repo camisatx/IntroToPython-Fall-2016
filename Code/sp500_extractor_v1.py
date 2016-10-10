6b31b2e3419c539ed8e62847901db45143f8a615
camisatx <joshschertz3@gmail.com>
2016-10-10 10:03:23 -0500

    Add sp500_extractor_v1 file with initial url links

---
+++ Code/sp500_extractor_v1.py
@@ -0,0 +1,19 @@
+
+"""
+Make it work, make it right, make it fast
+
+Extract the tickers from the S&P 500 table on Wikipedia and process them into
+a list. Then save the list into a CSV file.
+
+# Python 3.5 retrieve URLs
+https://docs.python.org/3/howto/urllib2.html#fetching-urls
+
+# HTML tables
+http://www.w3schools.com/html/html_tables.asp
+
+# BS4 parsing
+http://stackoverflow.com/a/23377804
+
+# Save to CSV
+http://gis.stackexchange.com/a/72476
+"""
